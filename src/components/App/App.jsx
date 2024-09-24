afea3becabc616a404c7dbf9503de1f60029f427
Oleksii Aleksieiev <reneguarde@gmail.com>
2024-09-24 20:01:54 +0300

    Init project structure and components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import css from './App.module.css';
+
+import ContactForm from '../ContactForm/ContactForm.jsx';
+import ContactList from '../ContactList/ContactList.jsx';
+import SearchBox from '../SearchBox/SearchBox.jsx';
+
+import { useEffect, useState } from 'react';
+
+const App = () => {
+  return (
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+};
+
+export default App;
